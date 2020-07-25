18f77595f8e8259e2434e20a8df5f3023243ecfa
Wojakposter <based.departament@wojakposting.su>
2020-07-25 14:33:05 +0200

    Remove unused function openInNewTab

--- Bunkersoy.user.js
+++ Bunkersoy.user.js
@@ -92,11 +92,6 @@
         }
     }
 
-    const openInNewTab = (base64Data) => {
-        let win = window.open();
-        win.document.write('<iframe src="' + base64Data + '" frameborder="0" style="border:0; top:0px; left:0px; bottom:0px; right:0px; width:100%; height:100%;" allowfullscreen></iframe>');
-    }
-
     const generateWojak = (postText) => {
         const image = new Image();
         const soyType = document.getElementById("soyjakSelector").value;
